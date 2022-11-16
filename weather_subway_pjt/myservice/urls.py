3563816200226aaa08e38bd8a907044ebd31fd59
hhongpiz <hhongpizza@gamil.com>
2022-11-17 00:02:04 +0900

    221117 commit

--- weather_subway_pjt/myservice/urls.py
+++ weather_subway_pjt/myservice/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('my_place/', views.my_place, name='my_place'),
     path('my_place_impl/', views.save_my_place, name='my_place'),
+    path('del/my_place/', views.del_my_place, name='my_place'),
 ]
--- weather_subway_pjt/myservice/views.py
+++ weather_subway_pjt/myservice/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from options.models import Tourism
 from .models import MyPlace
 from django.contrib.auth.decorators import login_required
 
@@ -9,8 +9,9 @@ def my_place(request):
     list_mp = None
     list_st = None
     id = request.user.id
+
     if request.method == 'GET':
-        list_mp=MyPlace.objects.filter(user_id=id).distinct().values('station_name','t_name')
+        list_mp=MyPlace.objects.filter(user_id=id).distinct().values('station_name','t_name', 'mp_idx', 'cate')
         list_st=MyPlace.objects.filter(user_id=id).distinct().values('station_name','gu')
 
     else: 
@@ -31,10 +32,37 @@ def save_my_place(request):
         gu = request.POST['gu']
         location = request.POST.getlist('location')
         location_cate = request.POST.getlist('location_cate')
+        id = request.user.id
+        list_t=MyPlace.objects.filter(user_id=id).values('t_name')
+        # # print(list_t)
+        l=[]
+        for i in range(len(list_t)):
+            l.append(list(list_t[i].values())[0])
+
 
         for i, name in enumerate(location):
-            MyPlace(user_id=id, gu=gu, station_name=station, t_name=name, cate=location_cate[i]).save()
+            if len(location) != 0:
+                if name not in l:
+                    #for i, name in enumerate(location):
+                   MyPlace(user_id=id, gu=gu, station_name=station, t_name=name, cate=location_cate[i]).save()
+            else:
+                MyPlace(user_id=id, gu=gu, station_name=station, t_name=name, cate=location_cate[i]).save()
     else:
         return redirect('/')
 
     return redirect('/myservice/my_place/')
+
+@login_required(login_url='accounts:log_in')
+def del_my_place(request):
+
+    if request.method == 'GET':
+        return redirect('/')
+
+    id = request.user.id
+    del_places = request.POST.getlist('del-loc')
+
+    MyPlace.objects.filter(user_id=id, mp_idx__in=del_places).delete()
+    return redirect('/myservice/my_place/')
+
+
+
